94c2fda76e26a10a6bf77add0c1c883eaecac339
YoussefAswad55 <youssefaswad@gmail.com>
2021-07-15 12:27:09 +0200

    Added internet connection check.
    Add a new variable ($internet) using 'Test-Connection' cmdlet. It tries to connect to 'google.com' and returns true or false, base on that I will determine whether to enter the main body or not.

--- MyDuck.ps1
+++ MyDuck.ps1
@@ -1,36 +1,59 @@
-﻿$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+﻿$scriptDir = '.\Data'
 $domainFiles = (Get-ChildItem -Path $scriptDir -recurse -filter domains*.txt | Group-Object -Property Directory)
 $tokenFiles = (Get-ChildItem -Path $scriptDir -recurse -filter token*.txt | Group-Object -Property Directory)
 
-
-while($true){
-    For ($i=0; $i -lt $domainFiles.Count; $i++) {
-        $domainFile = Get-Content ($domainFiles.Name +"\"+ $domainFiles.Group[$i])
-        $token = Get-Content ($tokenFiles.Name +"\"+ $tokenFiles.Group[$i])
-
-        $domains = $null 
-
-        Foreach($d in $domainFile){
-
-            if($domains -eq $null){
-                $domains = $d
-            }
-            else{
-                $domains = $domains +"," + $d
-            }
-        }
-        $url = "https://www.duckdns.org/update?domains=" + $domains + "&token=" + $token + "&ip=&verbose=true"
-        $response = Invoke-WebRequest -Uri $url
-        $responseStr = [System.Text.Encoding]::UTF8.GetString($response.Content)
-        $responseArr = $responseStr.Split([Environment]::NewLine) | ? {$_}
-        $responseTable = [PSCustomObject]@{
-                            'Success' = $(If ($responseArr[0] -eq "OK") {$true} Else {$false}) 
-                            'IP Address'  = $responseArr[1]
-                            'Changed'  = $(If ($responseArr[2] -eq "UPDATED") {$true} Else {$false})
-                         }
-        $responseTable
-                                
-    }
-    TIMEOUT /T 300
+while ($true)
+{
+	if ($internet = Test-Connection -Quiet -ComputerName www.google.com)
+	{
+		For ($i = 0; $i -lt $domainFiles.Count; $i++)
+		{
+			$domainFile = Get-Content ($domainFiles.Name + "\" + $domainFiles.Group[$i])
+			$token = Get-Content ($tokenFiles.Name + "\" + $tokenFiles.Group[$i])
+			
+			$domains = $null
+			
+			Foreach ($d in $domainFile)
+			{
+				
+				if ($domains -eq $null)
+				{
+					$domains = $d
+				}
+				else
+				{
+					$domains = $domains + "," + $d
+				}
+			}
+			$url = "https://www.duckdns.org/update?domains=" + $domains + "&token=" + $token + "&ip=&verbose=true"
+			$response = Invoke-WebRequest -Uri $url
+			$responseStr = [System.Text.Encoding]::UTF8.GetString($response.Content)
+			$responseArr = $responseStr.Split([Environment]::NewLine) | ? { $_ }
+			$responseTable = [PSCustomObject]@{
+				'Success' = $(If ($responseArr[0] -eq "OK") { $true }
+					Else { $false })
+				'IP Address' = $responseArr[1]
+				'Changed' = $(If ($responseArr[2] -eq "UPDATED") { $true }
+					Else { $false })
+			}
+			if (!$responseTable.Success)
+			{
+				Write-Host "Failed to Update!"
+			}
+			else
+			{
+				if ($responseTable.Changed)
+				{
+					Write-Host "Updated to" $responseTable.'IP Address'
+				}
+			}
+			$responseTable
+		}
+	}
+	else
+	{
+		Write-Host "No Internet Connection"
+	}
+	TIMEOUT /T 5
 }
 
